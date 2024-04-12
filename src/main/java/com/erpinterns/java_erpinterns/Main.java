2fa9f5b8a5d08ef249e1b4e7ed84ae8d0e2caa41
softfiredev <braikbraik06@gmail.com>
2024-04-12 14:47:19 +0100

    commit 12/04/2024

---
+++ src/main/java/com/erpinterns/java_erpinterns/Main.java
@@ -0,0 +1,24 @@
+package com.erpinterns.java_erpinterns;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HeadmasterForm.fxml")));
+        primaryStage.setTitle("Headmaster Form");
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
